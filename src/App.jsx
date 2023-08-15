9122750fd056142777da1155c9833fb94b9fefed
Niamh Mallott <niamhmallott@hotmail.co.uk>
2023-08-15 10:01:21 +0100

    Main body to seperated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,66 @@
 import React from 'react'
 
-console.log('This is also a test')
+
 
 function App() {
   return (
-    <div>hi</div>
-  )
+    <>
+      <div>
+        <h1>Jamming</h1>
+      </div>
+      <SearchBar />
 
+      <div>
+        <SearchResults />
+        <MyPlaylist />
+      </div>
+    </>
+  )
+}
+
+function SearchBar() {
+  return (
+    <div>
+      <form>
+        <input type="search" ></input>
+        <button>Search</button>
+      </form>
+    </div>
+  )
+}
+
+
+function SearchResults() {
+  const tracks = ['song 1', 'song 2', 'song 3']
+
+  return (
+    <div>
+      <h2>Results</h2>
+
+      <TrackList tracks={tracks} />
+    </div>
+  )
+}
+
+function MyPlaylist() {
+  const tracks = ['song 1', 'song 2', 'song 3']
+
+  return (
+    <div>
+      <h2>My Playlist</h2>
+      <TrackList tracks={tracks} />
+    </ div>
+  )
+}
+
+function TrackList({ tracks }) {
+  return <ul>
+    {tracks.map(name => <Track name={name} />)}
+  </ul>
+}
+
+function Track({ name }) {
+  return <li>{name}</li>
 }
 
 export default App
