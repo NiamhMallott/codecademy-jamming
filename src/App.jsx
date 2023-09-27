4c9de95729d1636a05d1b69fa1d02843a1f752d9
Niamh Mallott <niamhmallott@hotmail.co.uk>
2023-09-27 11:38:39 +0100

    create a function that adds the track to the playlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 
 function App() {
-  const [playlistTitle, setPlaylistTitle] = useState("bloopy");
+  const [playlistTitle, setPlaylistTitle] = useState("");
   const [playlistTracks, setPlaylistTracks] = useState([]);
   const [searchResults, setSearchResults] = useState([
     {
@@ -16,7 +16,12 @@ function App() {
       album: "Taylors Version",
       id: 1,
     },
-    { name: "Bonfire", artist: "Childish Gambino", album: "Camp", id: 2 },
+    {
+      name: "Bonfire",
+      artist: "Childish Gambino",
+      album: "Camp",
+      id: 2,
+    },
     {
       name: "Ceilings",
       artist: "Lizzy McAlpine",
@@ -24,6 +29,19 @@ function App() {
       id: 3,
     },
   ]);
+
+  const addTrack = useCallback(
+    (newTrack) => {
+      for (let i = 0; i < playlistTracks.length; i++) {
+        if (newTrack.id === playlistTracks[i].id) {
+          return;
+        }
+      }
+
+      setPlaylistTracks([...playlistTracks, newTrack]);
+    },
+    [playlistTracks]
+  );
 
   return (
     <div>
@@ -36,7 +54,7 @@ function App() {
       </div>
 
       <div className="flex justify-between gap-x-8 max-w-2xl mx-auto">
-        <SearchResults searchResults={searchResults} />
+        <SearchResults searchResults={searchResults} addTrack={addTrack} />
         <Tracklist
           playlistTitle={playlistTitle}
           playlistTracks={playlistTracks}
@@ -56,7 +74,7 @@ function SearchBar() {
   return <input className="w-full ring-2 rounded px-2 py-1 ring-purple-500" />;
 }
 
-function SearchResults({ searchResults }) {
+function SearchResults({ searchResults, addTrack }) {
   return (
     <div>
       <h2 className="pb-2 font-bold">Search Results</h2>
@@ -68,6 +86,7 @@ function SearchResults({ searchResults }) {
               artist={searchResult.artist}
               album={searchResult.album}
               id={searchResult.id}
+              addTrack={addTrack}
             />
           </li>
         ))}
@@ -97,13 +116,22 @@ function Tracklist({ playlistTitle, playlistTracks }) {
   );
 }
 
-function Track({ name, artist, album, id }) {
+function Track({ name, artist, album, id, addTrack }) {
   return (
     <div>
       <p>{name}</p>
       <p>{artist}</p>
       <p>{album}</p>
       <p>{id}</p>
+      {addTrack && (
+        <button
+          onClick={() => {
+            addTrack({ name, artist, album, id });
+          }}
+        >
+          Add
+        </button>
+      )}
     </div>
   );
 }
